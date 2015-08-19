bfe7dd70e0594c43f961d05444e6962165d1705d
github username <github email address>
2015-08-19 12:08:31 -0400

    sleeeep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  for i in num.downto(1)
+    puts "#{i} SECOND(S)!"
+    sleep(1)
+  end
+ 'HAPPY NEW YEAR!'
+end
+
+def countdown_with_sleep(num)
+  sleep num
 end
